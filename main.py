193018a62dbdb19b5dd0cdf6c455110df525226a
Bairu Ashok <2100031968@kluniversity.in>
2022-08-03 16:15:32 +0530

    commit

---
+++ main.py
@@ -0,0 +1,18 @@
+list = ('vicky', 31968, 9.26 , 'snehith', 9.5)
+tinylist = (32092 , 'snehith')
+
+print(list)
+print(tinylist * 2)
+print(list[0])
+print(list[1:3])
+print(list[2:6])
+print((list,tinylist) * 3)
+str = "vighnesh"
+print(len(str))
+s = 'pfsd cseh students'
+print(s.split(" "))
+print("_".join(s.split()))
+list1 = [17,45,56,78]
+print("Largest element is:", max(list1))
+total = sum(list1)
+print("sum of all elements in given list:", total)
